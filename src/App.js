9d0af3f4ccee214d062bff0074debafd10a9a9f9
nthY <n_hat.yalcin@outlook.com>
2023-09-08 10:06:18 +0300

    add flip animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import Main from './components/Main';
 import Nav from './components/Nav';
+import { useEffect } from 'react';
 
 function App() {
+
+    // after component mounted
+    useEffect(() => {
+      flipStarter();
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,3 +21,29 @@ function App() {
 }
 
 export default App;
+
+let currentCardFaceToggle = true; // meaining front; false meaning back
+
+function flipStarter() {
+  console.log("Flip Starter invoked...");
+
+  const inner_card = document.getElementById("rotating");
+
+  inner_card.onload = flip();
+
+  function flip() {
+    setInterval(()=> {
+      console.log("Flipping...");
+  
+      if(currentCardFaceToggle) {
+        inner_card.style.transform = "rotateX(180deg)";
+      }else {
+        inner_card.style.transform = "rotateX(0deg)";
+      }
+  
+      currentCardFaceToggle = !currentCardFaceToggle;
+  
+    }, 3000);
+  }
+
+}
