22c875fb45ef3831ce7e7da4dd8d3af4fe0d8f38
nthY <n_hat.yalcin@outlook.com>
2023-09-08 14:03:43 +0300

    try different padding or margin

--- src/components/UpperContent.js
+++ src/components/UpperContent.js
@@ -7,7 +7,7 @@ function UpperContent() {
         <div className="row h-100 px-0 text-center justify-content-between align-items-center align-items-sm-stretch">
 
             {/* <!-- GG-OEE --> */}
-            <div className="col-5 mb-2 bg-bellona shadow-lg rounded-4">
+            <div className="col-5 bg-bellona shadow-lg rounded-4">
                 <div className="row h-100">
                     <div className="col align-self-center">
                         <h1 className="">% 78,2</h1>
@@ -17,7 +17,7 @@ function UpperContent() {
             </div>
 
             {/* <!-- G-OEE-1S --> */}
-            <div className="col-5 mb-2 bg-bellona shadow-lg rounded-4">
+            <div className="col-5 bg-bellona shadow-lg rounded-4">
                 <div className="row h-100">
                     <div className="col align-self-center">
                         <h1 className="">% 81,2</h1>
