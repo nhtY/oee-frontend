faf34edc07cc6e1e9fcd0c7b0f71b4b9e93c1f79
nthY <n_hat.yalcin@outlook.com>
2023-09-12 09:56:24 +0300

    serve state management redux hook methods from one custom source

---
+++ src/app/hooks.js
@@ -0,0 +1,6 @@
+import {useDispatch, useSelector} from 'react-redux';
+
+// import them from the source and use everywhere
+export const useAppDispatch = () => useAppDispatch();
+export const useAppSelector = useSelector;
+
