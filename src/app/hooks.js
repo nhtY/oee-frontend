44de33dff9c7622725ca2136883380a97de846aa
nthY <n_hat.yalcin@outlook.com>
2023-09-12 09:56:54 +0300

    fix method name

--- src/app/hooks.js
+++ src/app/hooks.js
@@ -1,6 +1,6 @@
 import {useDispatch, useSelector} from 'react-redux';
 
 // import them from the source and use everywhere
-export const useAppDispatch = () => useAppDispatch();
+export const useAppDispatch = () => useDispatch();
 export const useAppSelector = useSelector;
 
