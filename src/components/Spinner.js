d94222934142c439c8f64ecc37bb32418ed1bf5a
nthY <n_hat.yalcin@outlook.com>
2023-09-12 17:01:06 +0300

    render a spinner while fething

---
+++ src/components/Spinner.js
@@ -0,0 +1,18 @@
+
+
+function Spinner() {
+
+
+    return (
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="spinner-border text-primary spinner-border-lg"
+                role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        
+    );
+}
+
+
+export default Spinner;
